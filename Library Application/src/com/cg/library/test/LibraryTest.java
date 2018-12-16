3bc1911193dd4ff87eee87f34a8347bf9d40680e
Aditya Narayan <adityanarayan22@gmail.com>
2018-12-16 23:05:12 +0530

    Add files via upload

---
+++ Library Application/src/com/cg/library/test/LibraryTest.java
@@ -0,0 +1,20 @@
+package com.cg.library.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.cg.library.bean.LibraryBean;
+
+public class LibraryTest 
+{
+	
+	@Test
+	public void test() 
+	{
+		LibraryBean libraryBean = new LibraryBean();
+		
+		fail("Not yet implemented");
+	}
+
+}
